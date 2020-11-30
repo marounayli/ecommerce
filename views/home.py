290a2bd9f17832c6244c62a7bfbbed08150837af
Sandro Ephrem <sandro.ephrem@gmail.com>
2020-11-30 20:21:03 +0200

    MEME UPDATE

--- views/home.py
+++ views/home.py
@@ -4,4 +4,6 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 
-home_layout = [html.Div([html.H3("Welcome to Camunzon !")], className='layout-container')]
+home_layout = [html.Div([html.H3("Welcome to Camunzon !"),
+html.Img(src=app.get_asset_url('meme1.png'), className='meme'),
+ html.Img(src=app.get_asset_url('meme2.png'), className='meme'), html.Img(src=app.get_asset_url('meme3.jpg'), className='meme'),], className='layout-container')]
